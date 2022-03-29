00fb9997b622b64da841daf36c91e1fa9ce07dbd
Troels Bjørnskov <troels@bjoernskov.org>
2022-03-29 21:21:03 +0200

    Remove timeout

--- .config/sublime-text/Packages/User/paste-zsh.py
+++ .config/sublime-text/Packages/User/paste-zsh.py
@@ -5,6 +5,6 @@ import time
 class PasteZshCommand(sublime_plugin.WindowCommand):
     def run(self):
         subprocess.Popen(['/usr/bin/pkill', 'zsh', '--signal=USR2']).wait()
-        time.sleep(0.07)
+        # time.sleep(0.07)
         subprocess.Popen(['swaymsg', '[app_id="^PopUp$"]', 'scratchpad', 'show,', 'fullscreen', 'disable,', 'move', 'position', 'center,', 'resize', 'set', 'width', '100ppt', 'height', '100ppt,', 'resize', 'shrink', 'up', '1100px,', 'resize', 'grow', 'up', '150px']).wait()
 
