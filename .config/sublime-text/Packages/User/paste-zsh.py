e7b4b407f8df524a3a737c616cdd576305a280bc
Troels Bjørnskov <troels@bjoernskov.org>
2022-03-29 19:49:19 +0200

    Decrease clipboard height

--- .config/sublime-text/Packages/User/paste-zsh.py
+++ .config/sublime-text/Packages/User/paste-zsh.py
@@ -6,5 +6,5 @@ class PasteZshCommand(sublime_plugin.WindowCommand):
     def run(self):
         subprocess.Popen(['/usr/bin/pkill', 'zsh', '--signal=USR2']).wait()
         time.sleep(0.07)
-        subprocess.Popen(['swaymsg', '[app_id="^PopUp$"]', 'scratchpad', 'show,', 'fullscreen', 'disable,', 'move', 'position', 'center,', 'resize', 'set', 'width', '100ppt', 'height', '100ppt,', 'resize', 'shrink', 'up', '1100px,', 'resize', 'grow', 'up', '337px']).wait()
+        subprocess.Popen(['swaymsg', '[app_id="^PopUp$"]', 'scratchpad', 'show,', 'fullscreen', 'disable,', 'move', 'position', 'center,', 'resize', 'set', 'width', '100ppt', 'height', '100ppt,', 'resize', 'shrink', 'up', '1100px,', 'resize', 'grow', 'up', '150px']).wait()
 
