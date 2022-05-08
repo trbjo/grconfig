1a08dbfad01472aac4c92ceae54bbf8c90ae5979
Troels Bjørnskov <troels@bjoernskov.org>
2022-05-08 22:30:03 +0200

    Various changes

--- .config/sublime-text/Packages/User/set_dictionary.py
+++ .config/sublime-text/Packages/User/set_dictionary.py
@@ -1,28 +1,29 @@
 import sublime_plugin
-from sublime import active_window
+from sublime import active_window, View
+from typing import Union
 
 class SetDanishDictCommand(sublime_plugin.WindowCommand):
     def is_enabled(self):
-        buf: View | None = active_window().active_view()
+        buf: Union[View,None] = active_window().active_view()
         if buf is None:
             return False
         return True
 
     def run(self) -> None:
-        buf: View | None = active_window().active_view()
+        buf: Union[View,None] = active_window().active_view()
         if buf is None:
             return
         buf.settings().set(key="dictionary", value="Dictionaries/da_DK.dic")
 
 class SetEnglishDictCommand(sublime_plugin.WindowCommand):
     def is_enabled(self):
-        buf: View | None = active_window().active_view()
+        buf: Union[View,None] = active_window().active_view()
         if buf is None:
             return False
         return True
 
     def run(self) -> None:
-        buf: View | None = active_window().active_view()
+        buf: Union[View,None] = active_window().active_view()
         if buf is None:
             return
         buf.settings().set(key="dictionary", value="Packages/Language - English/en_US.dic")
